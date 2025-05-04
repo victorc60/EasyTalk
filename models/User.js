942452edd2d9235070538eeaa1dc301408580ded
Victor <Victorio@Noutbuk-lizo4ka.local>
2025-05-05 00:00:14 +0300

    indexjs9

--- models/User.js
+++ models/User.js
@@ -1,27 +1,62 @@
 import { DataTypes } from 'sequelize';
 import { sequelize } from '../database/database.js';
 
+// Определение модели
 const User = sequelize.define('User', {
   telegram_id: {
     type: DataTypes.BIGINT,
     primaryKey: true,
-    allowNull: false
+    allowNull: false,
+    unique: true
   },
   username: {
     type: DataTypes.STRING,
+    allowNull: true,
+    validate: {
+      len: [3, 32]
+    }
+  },
+  first_name: {
+    type: DataTypes.STRING,
     allowNull: true
+  },
+  last_name: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  points: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+    validate: {
+      min: 0
+    }
+  },
+  is_active: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true
   },
   first_activity: {
     type: DataTypes.DATE,
+    allowNull: false,
     defaultValue: DataTypes.NOW
   },
   last_activity: {
     type: DataTypes.DATE,
+    allowNull: false,
     defaultValue: DataTypes.NOW
   }
 }, {
   timestamps: false,
-  tableName: 'users'
+  tableName: 'users',
+  indexes: [
+    {
+      fields: ['username']
+    },
+    {
+      fields: ['is_active']
+    }
+  ]
 });
 
+// Экспорт модели
 export default User;
