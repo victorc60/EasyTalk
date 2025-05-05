9e22a0d69b32a9344693fd4d49492a595cbef4f5
VictorTibuleac <105111283+victorc60@users.noreply.github.com>
2025-05-05 14:10:07 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,8 +1,8 @@
 import { DataTypes } from 'sequelize';
-import { sequelize } from '../database/database.js';
+import { syncDatabase } from '../database/database.js';
 
 // Определение модели
-const User = sequelize.define('User', {
+const User = syncDatabase.define('User', {
   telegram_id: {
     type: DataTypes.BIGINT,
     primaryKey: true,
