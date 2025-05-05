b9b6613cb411a5ab2c97fbbfcd268bc300803a0a
VictorTibuleac <105111283+victorc60@users.noreply.github.com>
2025-05-05 13:14:22 +0300

    Update schedule.js

--- config/schedule.js
+++ config/schedule.js
@@ -2,7 +2,7 @@ import schedule from 'node-schedule';
 import { dailyFactBroadcast, wordGameBroadcast, cleanupInactiveUsers } from '../services/adminServices.js';
 import { CONFIG } from './constants.js';
 
-export function setupSchedules() {
+export function setupSchedulesTasks() {
   schedule.scheduleJob(CONFIG.DAILY_FACT_TIME, dailyFactBroadcast);
   schedule.scheduleJob(CONFIG.WORD_GAME_TIME, wordGameBroadcast);
   schedule.scheduleJob(CONFIG.CLEANUP_TIME, cleanupInactiveUsers);
